2db68c63bb22d49ed855a3692a66e9bc1a509b7a
madison sorah <madisonsorah@gmail.com>
2022-02-02 15:01:29 -0500

    Built out HomePage component

--- client/src/components/HomePage/index.js
+++ client/src/components/HomePage/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import './index.css';
 
-function HomePage() {
+function HomePage({currentAuthor}) {
     return (
         <div>
+            {currentAuthor ? (
+                <div>
+                    <h1>Welcome back, Madison.</h1>
+                    <p>Ready to publish today's journal entry?</p>
+                    <Link to='/publish'>Write down your thoughts.</Link>
+                    <Link to='/account'>Update your account.</Link>
+                </div>
+            ) : (
+                <div>
+                    <h1>Welcome to Pixelpad.</h1>
+                </div>
+            )}
         </div>
     )
 }
