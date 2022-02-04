1665a8d72df9088c55d9627f6b37c0f040f2bb22
madison sorah <madisonsorah@gmail.com>
2022-02-03 19:41:40 -0500

    Created controllers for each model

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,17 @@ Rails.application.routes.draw do
   resources :authors, only: [:show, :create, :update, :destroy]
 
   # Journal Entry CRUD actions
-  resources :journal_entries, only: [:index, :show, :create, :update, :destroy]
+  resources :journal_entries, only: [:index, :show, :create, :update, :destroy] 
+
+  # Theme CRUD actions
+  resources :themes, only: [:index, :show, :update]
+
+  # Question CRUD actions
+  resources :questions, only: [:index, :show]
+
+  # Answer CRUD actions 
+  resources :answers, only: [:index, :show, :create, :update]
+
   
   get '*path',
       to: 'fallback#index',
